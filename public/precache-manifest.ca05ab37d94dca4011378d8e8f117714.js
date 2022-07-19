2ef981a8561f155b6d2e8e35bd05b7fe0813802e
umarsuhail112 <umarsuhail112@gmail.com>
2022-07-19 14:36:48 +0530

    latest changes

---
+++ public/precache-manifest.ca05ab37d94dca4011378d8e8f117714.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb6a0b5faf6d5b49adf7b190580c569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f353c98e46a23ffd897",
+    "url": "/static/css/main.f61c3e07.chunk.css"
+  },
+  {
+    "revision": "d1e8de39a6d1bd195deb",
+    "url": "/static/js/2.2d016ddc.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.2d016ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f353c98e46a23ffd897",
+    "url": "/static/js/main.eb75614c.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "90ccad7318ce12f2c4a10ce6e1726589",
+    "url": "/static/media/dig-marketing.90ccad73.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "1fced3cc2fede13a1fb3d2c9c8f60319",
+    "url": "/static/media/kap-logo.1fced3cc.png"
+  },
+  {
+    "revision": "730ee9585b3bec55039d101acc5dc061",
+    "url": "/static/media/kapnmob2.730ee958.jpg"
+  },
+  {
+    "revision": "376a3d9a1c0953e22900f97c5cd60ca2",
+    "url": "/static/media/kapnmob3.376a3d9a.jpg"
+  },
+  {
+    "revision": "5dd128698b609f79e527e9dcf037a957",
+    "url": "/static/media/video-thump.5dd12869.jpg"
+  }
+]);
